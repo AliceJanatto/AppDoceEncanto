67a9177be8b74932bfc24d51d1663bba1c05785a
Alice Janatto <janattoa@gmail.com>
2024-06-07 09:53:13 -0300

    add arquivos

---
+++ APPDoceria/APPDoceria/obj/Debug/net8.0-android/android/src/crc64cc56e92d0ef5e75f/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64cc56e92d0ef5e75f;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("APPDoceria.MainActivity, APPDoceria", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("APPDoceria.MainActivity, APPDoceria", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("APPDoceria.MainActivity, APPDoceria", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
